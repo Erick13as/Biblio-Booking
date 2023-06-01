962425237b19f2e4bb44aa80a5374c2fa54326d6
Sofiavega12 <sofiavegachavarria@gmail.com>
2023-05-31 22:36:36 -0600

    Cambio en fecha

--- app/src/main/java/com/example/biblio_booking/Registro.java
+++ app/src/main/java/com/example/biblio_booking/Registro.java
@@ -3,10 +3,13 @@ package com.example.biblio_booking;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,10 +19,13 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
+
 public class Registro extends AppCompatActivity {
 
     private FirebaseFirestore mFirestore;
-
+    private TextView editTextFecha;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +47,41 @@ public class Registro extends AppCompatActivity {
                 reOpenPrincipal();
             }
         });
+        editTextFecha = findViewById(R.id.editTextFecha);
+
+        // Set click listener for the date button
+        editTextFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+        // Initialize the date picker listener
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                // Handle the selected date here
+                String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                editTextFecha.setText(selectedDate);
+            }
+        };
+    }
+    private void showDatePickerDialog() {
+        // Get the current date
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Create and show the date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                dateSetListener,
+                year,
+                month,
+                dayOfMonth);
+        datePickerDialog.show();
     }
 
     private void uploadDataToFirestore() {
