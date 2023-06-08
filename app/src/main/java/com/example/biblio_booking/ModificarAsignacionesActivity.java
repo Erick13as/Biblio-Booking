7b30eb0f519bc11992d9aadf86f95b99f57c4d94
Erick13as <alvaradosolanoerick@gmail.com>
2023-06-07 22:58:34 -0600

    Update ModificarAsignacionesActivity.java

--- app/src/main/java/com/example/biblio_booking/ModificarAsignacionesActivity.java
+++ app/src/main/java/com/example/biblio_booking/ModificarAsignacionesActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class ModificarAsignacionesActivity extends AppCompatActivity {
 
+    Asignacion asignacion = new Asignacion("7:30","Cubiculo1","5","19/6/2023");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
